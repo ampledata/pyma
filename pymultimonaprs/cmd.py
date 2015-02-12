2cad51aceffcb70d019820273fc00781097cd94a
Greg Albrecht <gba@onbeep.com>
2015-02-12 15:14:07 -0800

    Rewrite:
    - Factored 'script' out to cmd module.
    - Updated setup.py.

---
+++ pymultimonaprs/cmd.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""pymultimonaprs Package."""
+
+__author__ = 'Greg Albrecht W2GMD <gba@gregalbrecht.com>'
+__copyright__ = 'Copyright 2015 OnBeep, Inc.'
+__license__ = 'GNU General Public License, Version 3'
+
+
+import argparse
+import json
+import logging
+import logging.handlers
+import signal
+import sys
+import threading
+import time
+
+
+from pymultimonaprs.multimon import Multimon
+from pymultimonaprs import beacon
+from pymultimonaprs.gate import IGate
+from pymultimonaprs.frame import APRSFrame, InvalidFrame
+
+
+def main():
+    parser = argparse.ArgumentParser(description='HF2APRS-IG Gateway.')
+    parser.add_argument('-c', dest='config', default='/etc/pymultimonaprs.json', help='Use this config file')
+    parser.add_argument('--syslog', action='store_true', help='Log to syslog')
+    parser.add_argument('-v', '--verbose', action='store_true', help='Log all traffic - including beacon')
+    args = parser.parse_args()
+
+    with open(args.config) as config_file:
+        config = json.load(config_file)
+
+    logger = logging.getLogger('pymultimonaprs')
+    loglevel = logging.DEBUG if args.verbose else logging.INFO
+    logger.setLevel(loglevel)
+
+    if args.syslog:
+        loghandler = logging.handlers.SysLogHandler(address = '/dev/log')
+        formater = logging.Formatter('pymultimonaprs: %(message)s')
+        loghandler.setFormatter(formater)
+    else:
+        loghandler = logging.StreamHandler(sys.stdout)
+        formatter = logging.Formatter('[%(asctime)s] %(levelname)+8s: %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+        loghandler.setFormatter(formatter)
+    logger.addHandler(loghandler)
+
+
+    def mmcb(tnc2_frame):
+        try:
+            frame = APRSFrame()
+            frame.import_tnc2(tnc2_frame)
+            if config['append_callsign']:
+                frame.path.extend([u'qAR', config['callsign']])
+            ig.send(frame)
+        except InvalidFrame:
+            logger.info('Invalid Frame Received.')
+            pass
+
+    def bc():
+        bcargs = {
+            'lat': config['beacon']['lat'],
+            'lng': config['beacon']['lng'],
+            'callsign': config['callsign'],
+            'table': config['beacon']['table'],
+            'symbol': config['beacon']['symbol'],
+            'comment': config['beacon']['comment'],
+            'ambiguity': config['beacon'].get('ambiguity', 0),
+        }
+        bcargs_status = {
+            'callsign': config['callsign'],
+            'status': config['beacon']['status'],
+        }
+        bcargs_weather = {
+            'callsign': config['callsign'],
+            'weather': config['beacon']['weather'],
+        }
+        while 1:
+            # Position
+            frame = beacon.get_beacon_frame(**bcargs)
+            if frame:
+                ig.send(frame)
+
+            # Status
+            frame = beacon.get_status_frame(**bcargs_status)
+            if frame:
+                ig.send(frame)
+
+            # Weather
+            frame = beacon.get_weather_frame(**bcargs_weather)
+            if frame:
+                ig.send(frame)
+
+            sleep(config['beacon']['send_every'])
+
+    logger.info('Starting pymultimonaprs')
+
+    ig = IGate(config['callsign'], config['passcode'], config['gateway'])
+    mm = Multimon(mmcb,config)
+
+    def signal_handler(signal, frame):
+        logger.info('Stopping pymultimonaprs')
+        ig.exit()
+        mm.exit()
+        sys.exit(0)
+
+    signal.signal(signal.SIGINT, signal_handler)
+    signal.signal(signal.SIGTERM, signal_handler)
+
+    # Start beacon in main thread
+    if config.get('beacon'):
+        bc()
+
+
+if __name__ == '__main__':
+    main()
--- setup.py
+++ setup.py
@@ -1,66 +1,82 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-from distutils.core import setup
-from distutils.command.install import install
-from distutils.dir_util import mkpath
+"""pymultimonaprs Package."""
+
+__author__ = 'Greg Albrecht W2GMD <gba@gregalbrecht.com>'
+__copyright__ = 'Copyright 2015 OnBeep, Inc.'
+__license__ = 'GNU General Public License, Version 3'
+
+
 import pkg_resources
 import os
 import sys
 import shutil
 
-class post_install(install):
-	def run(self):
-		root = getattr(self, "root", "/") or "/"
-		print "Using '%s' as root" % root
-		if root == "/":
-			if os.getuid() != 0:
-				print "ERROR: Can't install to root '/' without root permissions"
-				sys.exit(1)
-			# cleanup old egg-info files
-			try:
-				while True:
-					p = pkg_resources.get_distribution("pymultimonaprs")
-					for f in os.listdir(p.location):
-						if f.startswith("pymultimonaprs") and f.endswith(".egg-info"):
-							egg_info = os.path.join(p.location, f)
-							try:
-								print "Deleting old egg-info: %s" % egg_info
-								os.unlink(egg_info)
-							except:
-								pass
-					reload(pkg_resources)
-			except pkg_resources.DistributionNotFound:
-				pass
-		install.run(self)
-		# install config file
-		print ""
-		cd = os.path.dirname(os.path.realpath(__file__))
-		src = os.path.join(cd, "pymultimonaprs.json")
-		dest = os.path.join(root, "etc/pymultimonaprs.json")
-		dest_new = os.path.join(root, "etc/pymultimonaprs.json.new")
-		mkpath(os.path.dirname(dest))
-		if os.path.isfile(dest):
-			print "Warning: %s already exists! Saved new config file to %s" % (dest, dest_new)
-			shutil.copyfile(src, dest_new)
-		else:
-			print "Installing config file to %s" % dest
-			shutil.copyfile(src, dest)
+from distutils.core import setup
+from distutils.command.install import install
+from distutils.dir_util import mkpath
+
+
+class PostInstall(install):
+  def run(self):
+    root = getattr(self, "root", "/") or "/"
+    print "Using '%s' as root" % root
+    if root == "/":
+      if os.getuid() != 0:
+        print "ERROR: Can't install to root '/' without root permissions"
+        sys.exit(1)
+      # cleanup old egg-info files
+      try:
+        while True:
+          p = pkg_resources.get_distribution("pymultimonaprs")
+          for f in os.listdir(p.location):
+            if f.startswith("pymultimonaprs") and f.endswith(".egg-info"):
+              egg_info = os.path.join(p.location, f)
+              try:
+                print "Deleting old egg-info: %s" % egg_info
+                os.unlink(egg_info)
+              except:
+                pass
+          reload(pkg_resources)
+      except pkg_resources.DistributionNotFound:
+        pass
+    install.run(self)
+    # install config file
+    print ""
+    cd = os.path.dirname(os.path.realpath(__file__))
+    src = os.path.join(cd, "pymultimonaprs.json")
+    dest = os.path.join(root, "etc/pymultimonaprs.json")
+    dest_new = os.path.join(root, "etc/pymultimonaprs.json.new")
+    mkpath(os.path.dirname(dest))
+    if os.path.isfile(dest):
+      print "Warning: %s already exists! Saved new config file to %s" % (dest, dest_new)
+      shutil.copyfile(src, dest_new)
+    else:
+      print "Installing config file to %s" % dest
+      shutil.copyfile(src, dest)
 
 
 
 setup(
-	name='pymultimonaprs',
-	version='1.0.0',
-	license='GPL',
-	description='RF2APRS-IG Gateway',
-	author='Dominik Heidler',
-	author_email='dominik@heidler.eu',
-	url='http://github.com/asdil12/pymultimonaprs',
-	packages=['pymultimonaprs'],
-	scripts=['bin/pymultimonaprs'],
-	data_files=[
-		#('/etc', ['pymultimonaprs.json']),
-		('/usr/lib/systemd/system', ['pymultimonaprs.service']),
-	],
-	cmdclass={'install': post_install},
+    name='pymultimonaprs',
+    version='1.0.0g',
+    license='GNU General Public License, Version 3',
+    description='RF2APRS-IG Gateway',
+    author='Greg Albrecht',
+    author_email='gba@gregalbrecht.com',
+    url='http://github.com/ampledata/pymultimonaprs',
+    packages=['pymultimonaprs'],
+    data_files=[
+        #('/etc', ['pymultimonaprs.json']),
+        ('/usr/lib/systemd/system', ['pymultimonaprs.service']),
+    ],
+    entry_points={
+        'console_scripts': [
+            'pymultimonaprs = pymultimonaprs.cmd:main'
+        ]
+    },
+    cmdclass={'install': PostInstall},
+    zip_safe=False,
+    include_package_data=True
 )
