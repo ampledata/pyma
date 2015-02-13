24663084183f8354aef48590d6515fe7b7200862
Greg Albrecht <gba@onbeep.com>
2015-02-12 16:41:24 -0800

    Rewrite:
    - Added constants.

---
+++ pymultimonaprs/constants.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Constants for pymultimonaprs Module.
+"""
+
+__author__ = 'Greg Albrecht W2GMD <gba@gregalbrecht.com>'
+__copyright__ = 'Copyright 2015 OnBeep, Inc.'
+__license__ = 'GNU General Public License, Version 3'
+
+
+import logging
+
+
+LOG_LEVEL = logging.INFO
+LOG_FORMAT = ('%(asctime)s %(levelname)s %(name)s.%(funcName)s:%(lineno)d'
+              ' - %(message)s')
