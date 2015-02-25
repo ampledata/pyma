db8a7d9e406acb8b69c30d00b558000edf6c1f18
Greg Albrecht <gba@onbeep.com>
2015-02-24 17:00:50 -0800

    cleanup setup.py

--- setup.py
+++ setup.py
@@ -8,61 +8,10 @@ __copyright__ = 'Copyright 2015 OnBeep, Inc.'
 __license__ = 'GNU General Public License, Version 3'
 
 
-import pkg_resources
-import os
-import sys
-import shutil
-
-from distutils.command.install import install
-from distutils.dir_util import mkpath
-
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup  # pylint: disable=F0401,E0611
+import setuptools
 
 
-class PostInstall(install):
-  def run(self):
-    root = getattr(self, "root", "/") or "/"
-    print "Using '%s' as root" % root
-    if root == "/":
-      if os.getuid() != 0:
-        print "ERROR: Can't install to root '/' without root permissions"
-        sys.exit(1)
-      # cleanup old egg-info files
-      try:
-        while True:
-          p = pkg_resources.get_distribution("pymultimonaprs")
-          for f in os.listdir(p.location):
-            if f.startswith("pymultimonaprs") and f.endswith(".egg-info"):
-              egg_info = os.path.join(p.location, f)
-              try:
-                print "Deleting old egg-info: %s" % egg_info
-                os.unlink(egg_info)
-              except:
-                pass
-          reload(pkg_resources)
-      except pkg_resources.DistributionNotFound:
-        pass
-    install.run(self)
-    # install config file
-    print ""
-    cd = os.path.dirname(os.path.realpath(__file__))
-    src = os.path.join(cd, "pymultimonaprs.json")
-    dest = os.path.join(root, "etc/pymultimonaprs.json")
-    dest_new = os.path.join(root, "etc/pymultimonaprs.json.new")
-    mkpath(os.path.dirname(dest))
-    if os.path.isfile(dest):
-      print "Warning: %s already exists! Saved new config file to %s" % (dest, dest_new)
-      shutil.copyfile(src, dest_new)
-    else:
-      print "Installing config file to %s" % dest
-      shutil.copyfile(src, dest)
-
-
-
-setup(
+setuptools.setup(
     name='pymultimonaprs',
     version='2.0.0',
     license='GNU General Public License, Version 3',
@@ -71,16 +20,11 @@ setup(
     author_email='gba@gregalbrecht.com',
     url='http://github.com/ampledata/pymultimonaprs',
     packages=['pymultimonaprs'],
-    data_files=[
-        #('/etc', ['pymultimonaprs.json']),
-        ('/usr/lib/systemd/system', ['pymultimonaprs.service']),
-    ],
     entry_points={
         'console_scripts': [
             'pymultimonaprs = pymultimonaprs.cmd:main'
         ]
     },
-    cmdclass={'install': PostInstall},
     zip_safe=False,
     include_package_data=True
 )
